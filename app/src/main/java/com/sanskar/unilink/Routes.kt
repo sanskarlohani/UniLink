77da923b8d3b5beae99bd1357f2f55443b95a423
sanskar <sanskarlohani730@gmail.com>
2025-06-30 20:29:22 +0530

    Initialize Repository and Routes
    This commit introduces:
    - `Routes.kt`: Defines constants for navigation routes within the application.
    - `Repository.kt`: Implements a repository pattern for interacting with Firebase Authentication, Firestore, and Realtime Database. It includes functions for:
        - User authentication (login, signup, signout)
        - User profile management (edit profile)
        - Managing lost and found items (reporting, retrieving)

---
+++ app/src/main/java/com/sanskar/unilink/Routes.kt
@@ -0,0 +1,11 @@
+package com.sanskar.unilink
+
+object Routes {
+    const val LOGIN = "login"
+    const val SIGNUP = "signup"
+    const val HOME = "home"
+    const val REPORT = "report"
+    const val ITEM_DETAILS = "item_details"
+    const val PROFILE = "profile"
+    const val LOST = "lost"
+}
